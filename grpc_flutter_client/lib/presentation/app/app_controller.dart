36ce4c4c290dad8a5da1607b79ba375673ded48a
Elizaveta Zaitseva <lizazaytseva16@gmail.com>
2022-09-29 12:22:59 +0300

    add-channel-closure/refactoring

--- grpc_flutter_client/lib/presentation/app/app_controller.dart
+++ grpc_flutter_client/lib/presentation/app/app_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:grpc_flutter_client/domain/interactors/app_interactor.dart';
 
-class AppController extends FullLifeCycleController {
+class AppController extends GetxController {
   final AppInteractor _appInteractor;
 
   AppController(this._appInteractor);
